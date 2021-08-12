afbf44568573fcbec0197bf9e670990cd5db250b
shanart <shanin.x@gmail.com>
2021-08-12 12:58:09 +0300

    initial commit

---
+++ src/Main.cpp
@@ -0,0 +1,16 @@
+#include "headers/Game.h"
+
+int main(int argc, char* argv[])
+{
+	// Program entry point.
+	Game game; // Creating our game object.
+	
+    while(!game.GetWindow()->IsDone())
+    {
+		// Game loop.
+		game.HandleInput();
+		game.Update();
+		game.Render();
+		game.RestartClock();
+	}
+}
