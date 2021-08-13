4f0b49178018be80868c5fca257a17de0802f992
shanart <shanin.x@gmail.com>
2021-08-13 09:23:18 +0300

    complete Extend function

--- src/sources/Snake.cpp
+++ src/sources/Snake.cpp
@@ -1,13 +1,12 @@
 #include "../headers/Snake.h"
 
-
 Snake::Snake(int l_blockSize)
 {
     m_size = l_blockSize;
     m_bodyRect.setSize(sf::Vector2f(m_size - 1, m_size - 1));
     Reset();
 }
-Snake::~Snake(){}
+Snake::~Snake() {}
 
 void Snake::Reset()
 {
@@ -42,29 +41,61 @@ bool Snake::HasLost() { return m_lost; }
 void Snake::Lose() { m_lost = true; }
 void Snake::ToggleLost() { m_lost = !m_lost; }
 
-
 // When snake eat something it should grow - basicaly
 // we need to add new element to body
 void Snake::Extend()
 {
-    if (m_snakeBody.empty()) { return; }
+    if (m_snakeBody.empty())
+    {
+        return;
+    }
 
-    SnakeSegment& tail_head = m_snakeBody[m_snakeBody.size() - 1];
+    SnakeSegment &tail_head = m_snakeBody[m_snakeBody.size() - 1];
 
     if (m_snakeBody.size() > 1)
     {
-        SnakeSegment& tail_bone = m_snakeBody[m_snakeBody.size() - 2];
+        SnakeSegment &tail_bone = m_snakeBody[m_snakeBody.size() - 2];
 
-        if (tail_head.position.x == tail_bone.position.x) 
+        if (tail_head.position.x == tail_bone.position.x)
         {
             if (tail_head.position.y > tail_bone.position.y)
             {
                 m_snakeBody.push_back(SnakeSegment(tail_head.position.x, tail_head.position.y + 1));
-            } 
-            else 
+            }
+            else
             {
                 m_snakeBody.push_back(SnakeSegment(tail_head.position.x, tail_head.position.y - 1));
             }
+        }
+        else if (tail_head.position.y == tail_bone.position.y)
+        {
+            if (tail_head.position.x > tail_bone.position.x)
+            {
+                m_snakeBody.push_back(SnakeSegment(tail_head.position.x + 1, tail_head.position.y));
+            }
+            else
+            {
+                m_snakeBody.push_back(SnakeSegment(tail_head.position.x - 1, tail_head.position.y));
+            }
+        }
+    }
+    else
+    {
+        if (m_dir == Direction::Up)
+        {
+            m_snakeBody.push_back(SnakeSegment(tail_head.position.x, tail_head.position.y + 1));
+        }
+        else if (m_dir == Direction::Down)
+        {
+            m_snakeBody.push_back(SnakeSegment(tail_head.position.x, tail_head.position.y - 1));
+        }
+        else if (m_dir == Direction::Left)
+        {
+            m_snakeBody.push_back(SnakeSegment(tail_head.position.x + 1, tail_head.position.y));
+        }
+        else if (m_dir == Direction::Right)
+        {
+            m_snakeBody.push_back(SnakeSegment(tail_head.position.x - 1, tail_head.position.y));
         }
     }
 }
