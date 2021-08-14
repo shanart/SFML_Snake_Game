53587f0f55017875a04a61e31276dee796018b5a
shanart <shanin.x@gmail.com>
2021-08-14 14:21:25 +0300

    setup it all togather

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,13 +1,11 @@
-#include "headers/Game.h"
+#include "./headers/Game.h"
 
 int main(int argc, char *argv[])
 {
     // Program entry point.
-    Game game; // Creating our game object.
-
+    Game game;
     while (!game.GetWindow()->IsDone())
     {
-        // Game loop.
         game.HandleInput();
         game.Update();
         game.Render();
